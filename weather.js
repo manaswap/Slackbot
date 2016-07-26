59652193a38c0c67480ac6841b5e565904d098b8
Swapneel Chalageri <swapneel96@gmail.com>
2016-07-26 11:08:28 -0400

    initial commit

---
+++ weather.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var request = require('request');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.set('port', (process.env.PORT || 9001));
+
+app.set('port', 8080);
+
+app.get('/', function(req, res){
+  res.send('It Works!');
+});
+
+//__________________________________________________
+
+app.post('/post', function(req, res){
+  var parsed_url = url.format({
+    pathname: 'http://api.openweathermap.org/data/2.5/weather',
+    query: {
+      zip: req.body.text
+      appid: provess.env.warn.WEATHER_ACCESS,
+    }
+  });
+
+  request(parsed_url, function(error, response, body){
+    if (!error && response.statusCode == 200){
+      var data = JSON.parse(body);
+      var first_url = data.response.hits[0].result.url;
+
+      var body = {
+        response_type: "in_channel",
+        text: first_url
+      };
+
+      res.send(body);
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+   console.log('Node app is running on port', app.get('port'));
+});
